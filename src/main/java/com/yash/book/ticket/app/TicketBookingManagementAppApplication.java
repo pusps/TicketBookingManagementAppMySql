e25963f6605d6e912db54e9175361e19b04f2412
Puspendra Kumar Sharma <puspendra.sharma@yash.com>
2019-06-05 11:36:58 +0530

    new changes

--- src/main/java/com/yash/book/ticket/app/TicketBookingManagementAppApplication.java
+++ src/main/java/com/yash/book/ticket/app/TicketBookingManagementAppApplication.java
@@ -2,11 +2,19 @@ package com.yash.book.ticket.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 
 @SpringBootApplication
-public class TicketBookingManagementAppApplication{
+public class TicketBookingManagementAppApplication extends SpringBootServletInitializer{
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	         return application.sources(TicketBookingManagementAppApplication.class);
+	    }
+
+
 	public static void main(String[] args) {
 	SpringApplication.run(TicketBookingManagementAppApplication.class, args);
 	
